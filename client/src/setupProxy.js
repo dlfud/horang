94c29d05334f46c85d3c7344647d7a7c979d1cac
dlfud <99ysl@naver.com>
2022-09-28 21:24:45 +0900

    3000포트 화면은 나오지만 오류남

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,8 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
-    proxy("/api", {
+    createProxyMiddleware("/api", {
       //도메인 api로 호출
       target: "http://localhost:5000/", //통신할 서버의 도메인주소
       changeOrigin: true,
